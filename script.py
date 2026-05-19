63f8b4d15e9209e085dfe483dcc9fe5b53d4ea6e
2smra <juan.jose.rt35@gmail.com>
2026-05-19 09:36:43 +0200

    Actualización automática

--- script.py
+++ script.py
@@ -24,4 +24,5 @@ subprocess.run(["git", "commit", "-m", "Actualización automática"])
 # Push a GitHub
 subprocess.run(["git", "push"])
 
+subprocess.run(["git", "pull"])
 print("✔ GitHub actualizado")
